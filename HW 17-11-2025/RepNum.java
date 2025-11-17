dcf83b029b55214ae57d7619fdcc41e25cd10576
SethuGoldking <goldkingsethu@gmail.com>
2025-11-17 20:38:19 +0530

    Add files via upload

---
+++ HW 17-11-2025/RepNum.java
@@ -0,0 +1,24 @@
+class RepNum
+{
+	public static void main (String[] args)
+	{
+		int ar[] = {10,5,3,4,3,5,6};
+		RepNum rp = new RepNum();
+		System.out.println(rp.num(ar));
+	}
+	int num (int ar[])
+	{
+		for (int i=0; i<ar.length;i++)
+		{
+			for (int j=i+1; j<ar.length;j++)
+			{
+				if (ar[i]==ar[j])
+				{
+					return ar[i];
+				}
+			}
+		}
+		return -1;
+	}
+	
+}
